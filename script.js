efc695d60f29776b2981fbd98fa93fc7d768ca8b
DilochanNeupane <dilochanneupane@gmail.com>
2024-08-24 17:36:43 +0545

    html.inex

--- script.js
+++ script.js
@@ -46,7 +46,8 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
-    
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
 
     footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
